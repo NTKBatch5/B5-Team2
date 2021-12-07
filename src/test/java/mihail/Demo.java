a22067a1b1a7f80a4548f3b92f29d7100b9de347
Mihail <Mihail@LAPTOP-DTKS6POR.fios-router.home>
2021-12-07 17:40:19 -0500

    Added ExerciseTwo

--- src/test/java/mihail/Demo.java
+++ src/test/java/mihail/Demo.java
@@ -29,8 +29,40 @@ public class Demo {
 		driver.findElement(By.id("submit")).click();
 
 		Thread.sleep(5000);
+	
+	url = "http://demo.guru99.com/test/newtours/";
 
-		driver.close();
+	driver.get(url);
+	
+	driver.findElement(By.linkText("REGISTER")).click();
+
+	driver.findElement(By.name("firstName")).sendKeys("Mihail");
+
+	driver.findElement(By.name("lastName")).sendKeys("Stegariov");
+	
+	driver.findElement(By.name("phone")).sendKeys("240-393-5816");
+	
+	driver.findElement(By.name("userName")).sendKeys("mihail.aquasafe@gmail.com");
+	
+	driver.findElement(By.name("address1")).sendKeys("14801 Hammersmith Circle");
+	
+	driver.findElement(By.name("city")).sendKeys("Silver Spring");
+	
+	driver.findElement(By.name("state")).sendKeys("Maryland");
+	
+	driver.findElement(By.name("postalCode")).sendKeys("20906");
+	
+	driver.findElement(By.name("email")).sendKeys("mihail.aquasafe@gmail.com");
+	
+	driver.findElement(By.name("password")).sendKeys("Sasha09092018");
+	
+	driver.findElement(By.name("confirmPassword")).sendKeys("Sasha09092018");
+	
+	driver.findElement(By.name("submit")).click();
+
+	Thread.sleep(5000);
+
+	driver.close();
+
 	}
- 
 }
