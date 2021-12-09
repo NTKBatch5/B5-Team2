93f7654257a903cd70d79d9938168ab433812e40
Diana-Banana <DianaStrezhesku@gmail.com>
2021-12-09 00:31:37 -0500

    Commit getName method. Diana 1

--- src/test/java/diana/Demo.java
+++ src/test/java/diana/Demo.java
@@ -1,15 +1,15 @@
 package diana;
 
 public class Demo {
-	
 
 	public static void main(String[] args) {
-	
-  
-	
-  
-		}
-	}
-	
- 
 
+		getName("Diana");
+
+	}
+
+	public static void getName(String name) {
+		System.out.println(name);
+
+	}
+}
