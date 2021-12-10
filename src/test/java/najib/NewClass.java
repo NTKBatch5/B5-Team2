764fc33810a4e2cafc40be947d4393f57ebe285f
Diana-Banana <DianaStrezhesku@gmail.com>
2021-12-09 21:34:15 -0500

    Diana added "createCodes method"

--- src/test/java/najib/NewClass.java
+++ src/test/java/najib/NewClass.java
@@ -4,8 +4,14 @@ public class NewClass {
 
 	public static void main(String[] args) {
 		
+		createCodes("Blah-Blah");
+		
 		System.out.println("Creating new Class");
 
+	}
+	
+	public static void createCodes(String code) {
+		System.out.println(code);
 	}
 
 }
